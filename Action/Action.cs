c7629b67825610077188725d543a7fe8f47f41f4
yeonseong shim <sim5522@naver.com>
2022-03-09 23:51:36 +0700

    Add Action, Func delegate 학습

---
+++ Action/Action.cs
@@ -0,0 +1,43 @@
+﻿/// <summary>
+/// Action 델리게이트
+/// - 중요 특징 : 리턴 값이 없어야 한다.
+/// - 파라미터는 0 ~ 16개까지 가능하다
+///     - 0개 : Action delegate
+///     - 1개 : Action<T> delegate
+///     - 2개 : Action<T, T> delegate
+/// - 기타 : 많은 함수의 경우 대개 3~5개의 파라미터가 있다.
+/// 
+/// </summary>
+namespace Action
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            // Action<T>
+            // 입력 : T 타입
+            // 리턴값 : 없음
+
+            // 기존 메서드 지정
+            System.Action<String> act = Output;
+            act("Hello");
+
+            // 무명 메서드 지정
+            Action<string, string> act2 = delegate (string msg, string title)
+            {
+                Console.WriteLine($"{title} - {msg}");
+            };
+            act2("No data found", "Error");
+
+            // 람다식 사용
+            Action<int> act3 = code => Console.WriteLine($"Code : {code}");
+            act3(1004);
+        }
+
+        static void Output(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
+
---
+++ Func/Func.cs
@@ -0,0 +1,55 @@
+﻿using static System.Console;
+/// <summary>
+/// Func 델리게이트
+/// - 주요 특징 : 반드시 리턴 타입이 존재 한다.
+/// - 파라미터는 0 ~16개까지 가능하다
+///     - 0개 : Func<T> delegate => 이경우 T는 리턴값의 타입을 가리킨다.
+///     - 1개 : Func<T, TResult> delegate
+///     - 2개 : Func<T1, T2, TResult> delegate
+/// </summary>
+namespace Func
+{
+    static class MainApp
+    {
+        static void Main(string[] args)
+        {
+            // 메서드 지정
+            System.Func<bool> f = IsValid;
+            bool result = f();
+
+            System.Func<int, bool> c = IsValidRange;
+            bool result2 = c(10);
+
+            // 무명 메서드 지정
+            Func<int> fa = delegate
+            {
+                return _state == 0;
+            };
+            result = fa();
+
+            Func<int, bool> c2 = delegate (int n)
+            {
+                return n > 0;
+            };
+            result2 = c2(-1);
+
+            // 람다식 이용
+            Func<int> fb = () => _state == 0;
+            result = fb();
+
+            Func<int, bool> c3 = n => n > 0;
+            result2 = c3(-2);
+        }
+
+        private static bool IsValid()
+        {
+            return _state == 0;
+        }
+
+        private static bool IsValidRange(int n)
+        {
+            return n > 0;
+        }
+    }
+}
+
