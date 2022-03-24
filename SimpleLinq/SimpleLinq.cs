64f67fb2c880a2b7d922675e6d43a0fb8a05fb5c
yeonseong shim <shimyseong@gmail.com>
2022-03-24 23:46:46 +0700

    Add select문 무명 형식으로 새로운 형식 만들기 실습

---
+++ SimpleLinq/SimpleLinq.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleLinq
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Profile[] arrProfile =
+            {
+                new Profile(){ Name = "제니", Age = 23},
+                new Profile(){ Name = "지수", Age = 25},
+                new Profile(){ Name = "로제", Age = 22},
+                new Profile(){ Name = "리사", Age = 21}
+            };
+
+            var profiles = from profile in arrProfile
+                           where profile.Age < 22
+                           orderby profile.Age
+                           select new
+                           {
+                               Name = profile.Name,
+                               Age = profile.Age * 0.9
+                           };
+
+            foreach (var profile in profiles)
+                Console.WriteLine($"{profile.Name}, {profile.Age}");
+        }
+    }
+}
+
