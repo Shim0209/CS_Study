8f2ed3500c7fd704e07c6f5d3c840c7eef4a1fe0
yeonseong shim <shimyseong@gmail.com>
2022-04-09 20:13:27 +0700

    Add Boxing, Unboxing 설명

--- BoxingUnboxing/BoxingUnboxing.cs
+++ BoxingUnboxing/BoxingUnboxing.cs
@@ -1,5 +1,12 @@
 ﻿using System;
 
+/// Boxing
+/// - Value Type의 값을 캐스팅을 통해 Reference Type으로 변경할 경우, Reference Type의 객체는 새 객체를 Managed Heap에 만들고, 스택의 값을 Heap에 복사하는데, 이를 Boxing이라 한다.
+/// 
+/// Unboxing
+/// - 반대로 Boxing된 값을 Heap에서 Stack으로 Value Type으로 복원하는 과정은 Unboxing이라고 한다.
+/// 
+/// Boxing/Unboxing을 대량의 데이터 구조에서 자주 발생시키면, 성능을 크게 저하시키는 요인이 된다.
 namespace BoxingUnboxing
 {
     class MainApp
