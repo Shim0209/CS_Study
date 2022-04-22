c79c5d113a31652ce4b7f61cec15f55fbd54dbcb
yeonseong shim <shimyseong@gmail.com>
2022-04-22 20:34:59 +0700

    Add Thread Aborting (C#에서 지원끊음)

---
+++ SerialApp/SerialApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace SerialApp
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            // 1. SerialPort 클래스 객체 생성 (COM5 포트를 사용)
+            SerialPort
+
+            // 2. 시리얼포트 오픈
+
+            // 3. 시리얼포트에서 한 라인 읽기
+
+            // 4. 시리얼포트 닫기
+        }
+    }
+}
