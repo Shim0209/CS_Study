d62d2c108b4ebf2f3dfbd50d7e908bc65a6a546c
yeonseong shim <shimyseong@gmail.com>
2022-04-22 20:55:57 +0700

    Update Enum 프로젝트에 Flags 애트리뷰트 사용법 추가

--- Enum/Program.cs
+++ Enum/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 
+// Enum
+// - 열거 형식의 요소가 어떤 값을 갖느냐는 별 의미가 없다.
+// - 열거 형식의 각 요소는 서로 중복되지 않는 값을 갖고 있다는 데 의미가 있다.
 namespace Enum
 {
     class MainApp
     {
-        /*
-         * 열거 형식의 요소가 어떤 값을 갖느냐는 별 의미가 없다.
-         * 열거 형식의 각 요소는 서로 중복되지 않는 값을 갖고 있다는 데 의미가 있다.
-         */
         enum DialogResult { YES, NO, CANCEL, CONFIRM, OK }
         enum DialogType { YES=10, NO, CANCEL, CONFIRM=50, OK }
-
+        
+        [Flags]
+        enum MyEum
+        {
+            Apple = 1 << 0,
+            Orange = 1 << 1,
+            Kiwi = 1 << 2,
+            Mango = 1 << 3
+        };
         static void Main(string[] args)
         {
             Console.WriteLine((int)DialogResult.YES);
@@ -33,6 +40,14 @@ namespace Enum
             Console.WriteLine((int)DialogType.CANCEL);
             Console.WriteLine((int)DialogType.CONFIRM);
             Console.WriteLine((int)DialogType.OK);
+
+            // [Flags] 애트리뷰트를 갖는 열거형은 요소들의 집합으로 구성되는 값들도 표현할 수 있다.
+            Console.WriteLine((MyEum)1);
+            Console.WriteLine((MyEum)2);
+            Console.WriteLine((MyEum)4);
+            Console.WriteLine((MyEum)8);
+            Console.WriteLine((MyEum)(1 | 4)); // Apple, Kiwi   (Flags 사용x -> 결과값 : 5)
+            Console.WriteLine((MyEum)(1 | 8)); // Apple, Mango  (Flags 사용x -> 결과값 : 9)
         }
     }
 }
